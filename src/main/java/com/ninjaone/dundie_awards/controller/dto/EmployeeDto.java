4abf995d76903bd56d89fba6ce774f3ab142d79c
LUIS MARCILIO BRAGA DE OLIVEIRA <lmarcilio@stavvy.com>
2024-08-28 10:40:57 -0300

    feat: add Company to EmployeeDto

--- src/main/java/com/ninjaone/dundie_awards/controller/dto/EmployeeDto.java
+++ src/main/java/com/ninjaone/dundie_awards/controller/dto/EmployeeDto.java
@@ -1,15 +1,23 @@
 package com.ninjaone.dundie_awards.controller.dto;
 
 import com.ninjaone.dundie_awards.model.Employee;
+import com.ninjaone.dundie_awards.model.Organization;
 
 public record EmployeeDto(
     long id,
     String firstName,
     String lastName,
-    Integer dundieAwards
+    Integer dundieAwards,
+    OrganizationDto organization
 
 ) {
     public EmployeeDto(Employee employee) {
-        this(employee.getId(), employee.getFirstName(), employee.getLastName(), employee.getDundieAwards());
+        this(employee.getId(), employee.getFirstName(), employee.getLastName(), employee.getDundieAwards(), new OrganizationDto(employee.getOrganization()));
+    }
+
+    public record OrganizationDto(long id, String name) {
+        public OrganizationDto(Organization organization) {
+            this(organization.getId(), organization.getName());
+        }
     }
 }
